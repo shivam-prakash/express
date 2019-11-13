7088afc86beb2743841b5020e6a85875bd5cd77f
shivam-prakash <prakashshivam9gmail.com>
2019-11-13 18:10:03 +0000

    another dog route added

--- simple_express/app.js
+++ simple_express/app.js
@@ -3,10 +3,12 @@ var app=express();
 app.get("/",function(req,res){
 	res.send("this is root");
 })
-app.get("/dog",function(req,res){
+app.get("/cat",function(req,res){
 	res.send("meow");
-	
-})
+});	
+app.get("/dog",function(req,res){
+	res.send("bow bow");
+});
 app.listen(3000,function(){
 	console.log("server started");
 });
