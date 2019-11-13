5d587ff49a08bbf7468e8dc805dabaee1fe0e8da
shivam-prakash <prakashshivam9gmail.com>
2019-11-13 18:53:57 +0000

    usage of ejs

---
+++ simple_express_2/app.js
@@ -0,0 +1,23 @@
+var express=require("express");
+var app=express();
+app.get("/",function(req,res){
+	//res.send("hi there welcome to my assignment");
+res.render("app.ejs");
+})
+
+app.get("/request/:thing",function(req,res){
+	var thing=req.params.thing;
+	res.render("love.ejs",{thing:thing});
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+	console.log("starting new server");
+})
